30f2412fc89fc65885129025a071c1d5b50f758c
Dilmurod <126846397+DilshodovDilmurod@users.noreply.github.com>
2024-02-16 10:49:38 +0500

    add something

--- main.py
+++ main.py
@@ -1,56 +1,53 @@
 import random
-
 def sontop(x=10):
-    tasodifiy_son=random.randint(1, x)
-    print(f"Men 1 dan {x} gacha son o'yladim. Topa olasizmi.")
-    taxminlar = 0
+    tasodifiy_son = random.randint(1, x)
+    print(f"Men 1 dan {x} gacha son o'yladim topa olasizmi?")
+    tahminlar = 0
     while True:
-        taxminlar += 1
-        taxmin= int(input(">>>"))
-        if taxmin<tasodifiy_son:
-            print("Xato. Men o'ylagan son bundan katta. Yana harakat qiling:")
-        elif taxmin>tasodifiy_son:
-            print("Xato. Men o'ylagan son bundan kichik. Yana harakat qiling:")
+        tahminlar += 1
+        tahmin = int(input(">>>"))
+        if tahmin < tasodifiy_son:
+            print("Xato, Men oylagan son bundan kattaroq. Yana harakat qiling.")
+        elif tahmin > tasodifiy_son:
+            print("Xato, Men oylagan son bundan kichikroq. Yana harakat qiling.")
         else:
             break
-    print(f"Tabriklaymiz, {taxminlar} ta taxmin bilan to'gri topdingiz")
-    return taxminlar
-# print(sontop())
-def  son_top_pc(x=10):
-    input(f"1 dan {x} gacha son o'ylang va istalgan tugmani bosing. Men topaman.")
+    print(f"Tabriklayman siz {tahminlar} ta tahmin bilan topdingiz")
+    return tahminlar
+
+def sontop_pc(x=10):
+    input(f"1 dan {x} gacha istalgan son o'ylang va istalgan tugmani bosing. Men topaman")
     quyi = 1
     yuqori = x
-    taxminlar=0
+    tahminlar = 0
     while True:
-        taxminlar +=1
-        if yuqori != quyi:
-            taxmin= random.randint(quyi, yuqori)
+        tahminlar +=1
+        if quyi != yuqori:
+            tahmin = random.randint(quyi, yuqori)
         else:
-            taxmin = quyi
-        javob = input(f"Siz {taxmin} sonni o'yladingiz: tog'ri(t),"
-                      f"men oylagan son bundan kattaroq(+), yoki kichikroq(-)".lower())
-        if javob == "+":
-            quyi = taxmin+1
-        elif javob == "-":
-            yuqori=taxmin-1
+            tahmin = quyi
+        javob = input(f"Siz o'ylagan son {tahmin} ga teng, to'g'ri(t)"
+                      f"men o'ylagan son bundaan katta(+), yoki kichik (-)")
+        if javob == "-":
+            yuqori = tahmin-1
+        elif javob == "+":
+            quyi = tahmin+1
         else:
             break
-    print(f"Men {taxminlar} ta taxmin bilan to'gri topdim ")
-    return taxminlar
+    print(f"Men {tahminlar} ta tahmin bilan topdim")
+    return tahminlar
 
-def play (x=10):
-    while True:
-            tahmin_user = sontop(x)
-            tahmin_pc = son_top_pc(x)
-            if tahmin_pc>tahmin_user:
-                print("Siz yutdingiz")
-            elif tahmin_pc<tahmin_user:
-                print("Men yutdim")
-            else:
-                print("Durrang!")
-            javob = int(input("YAana o'ynaysizmi? Ha(1)/Yoq(0)"))
-            if javob == 0:
-                break
-            elif javob == 1:
-                continue
-print(play())    
+def play(x=10):
+    javob = True
+    while javob:
+        tahminlar_users = sontop(x)
+        tahminlar_pc = sontop_pc(x)
+        if tahminlar_users>tahminlar_pc:
+            print(f"Men {tahminlar_pc} ta tahmin yutdim!")
+        elif tahminlar_users < tahminlar_pc:
+            print (f"Siz {tahminlar_users} ta tahmin bilan yutdingiz!")
+        else:
+            print("Durrang!")
+        javob = int(input("Yana o'ynaysizmi?Ha(1)/Yoq(0)"))
+print(play())
+    
